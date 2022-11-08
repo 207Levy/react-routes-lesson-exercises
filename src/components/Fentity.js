496d159fcade8c374746ba65a86663fe176486df
Or <orlevy11@gmail.com>
2022-11-08 15:22:58 +0200

    DONE

--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -4,6 +4,11 @@ import '../styles/fentity.css'
 
 class Fentity extends Component {
     render() {
+        console.log(this.props.match)
+        const fentitiesCategory = this.props.match.params.fentities;
+        const fentityName = this.props.match.params.name;
+
+        const fentity = this.props.fentities[fentitiesCategory].find(f => f.name === fentityName);
         return (
             <div id="creature-container">
                 <h1>{fentity.name}</h1>
